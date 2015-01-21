c1f8e6556323d16d8619d6a64a484c541f5e5383
danbjoseph <danbjoseph@gmail.com>
2015-01-21 17:51:40 -0500

    show scene extents on map, sort by date, download options

--- js/main.js
+++ js/main.js
@@ -67,7 +67,14 @@ geocoder.markGeocode = function(result) {
 // ==================
 // initialise the FeatureGroup to store editable layers
 var drawnItems = new L.FeatureGroup();
-map.addLayer(drawnItems);
+// layer added after d3 scene extents group for ordering/display purposes
+
+var shapeOptions = {
+  clickable: false,
+  color: '#7a0177',
+  fill: false
+};
+
 // set options
 var options = {
     draw: {
@@ -75,12 +82,11 @@ var options = {
         circle: false,
         marker: false,
         polygon: {
-            allowIntersection: false // Restricts shapes to simple polygons
+            allowIntersection: false, // Restricts shapes to simple polygons
+            shapeOptions: shapeOptions
         },
         rectangle: {
-            shapeOptions: {
-                clickable: false
-            }
+            shapeOptions: shapeOptions
         }
     },
     edit: {
@@ -107,12 +113,55 @@ map.on('draw:created', function (e) {
 });
 
 
+// d3 overlay for scene boxes
+// ==========================
+function projectPoint(x, y) {
+  var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+  this.stream.point(point.x, point.y);
+}
+var transform = d3.geo.transform({point: projectPoint}),
+    path = d3.geo.path().projection(transform);
+
+// initialize the SVG layer for D3 drawn survey points
+map._initPathRoot()
+
+// pick up the SVG from the map object
+var svg = d3.select("#map").select("svg");
+// add leaflet draw layer
+map.addLayer(drawnItems);
+var sceneGroup = svg.append('g').attr("id", "d3scenes");
+
+
+
+function drawSceneBounds(data){
+  var sceneData = data;
+  var mappedScenes = sceneGroup.selectAll("path").data(sceneData, function(d){ return d['id']; });
+  mappedScenes.enter().append("path")
+    .attr("class", "scene-default")
+    .attr("d",path)
+    .on("mouseover", function(d){
+      console.log(d);           
+    })
+    // .on("mouseout", function(d){ 
+    //   $('#tooltip').empty();
+    // })
+  function updateScenePaths(){
+    mappedScenes.attr("d", path);
+  }
+  map.on("viewreset", updateScenePaths);
+  // remove scenes outside of new bounding box
+  mappedScenes.exit().remove();
+}
+
+
+
+
+
 
 // planet labs API scene search
+// ============================
 function searchScenes(aoi){
-
   $("#loading-wrapper").fadeIn();
-  
   // stringigy the polygon search area drawn to leaflet map
   var intersects = JSON.stringify(aoi);
   // set search parameters
@@ -134,32 +183,72 @@ function searchScenes(aoi){
 
           // log number of results displayed
           d3.select('#info-scene-count').html(data.features.length + " results");
+          // show sort tools
+          $("#info-sort-tools").show();
+
           // update listed scenes
           var results = d3.select('#info-scene-list').selectAll('div')
             .data(data.features, function(d){ return d['id']; });
           results.enter().append('div').html(function(d) { return generateSceneHtml(d); }).classed('scene-box', true);
           results.exit().remove();
 
+          // scenes may be in new and old selection
+          // the list is not completey refreshed, only updated
+          // so needs to be resorted in descending order
+          sortOrder = "desc";
+          d3.select('#info-scene-list').selectAll('.scene-box').sort(function(a,b){
+            return new Date(b.properties.acquired) - new Date(a.properties.acquired);
+          });
+
+          // draw polygons on map using d3
+          drawSceneBounds(data.features);
+
+          // hide loading gif overlay
           $("#loading-wrapper").fadeOut(500);
-
-            // need to sort them
-
-            //  >>>>>>>>> draw all polygons on map using d3
-
-            //  >>>>>>>>> close loading box
 
       }
       //  >>>>>>>>> if error code
   });
 }
 
+var sortOrder = "";
+
 function generateSceneHtml(sceneObject) {
   var sceneId = sceneObject.id;
   var acquired = formatAcquiredTime(new Date(sceneObject.properties.acquired));
+  var cloudCover = sceneObject.properties.cloud_cover.estimated;
   var sceneHtml = "<span class='text-key'>Scene ID:</span> <span class='text-value'>" + sceneId +
-    "</span><br><span class='text-key'>Acquired:</span> <span class='text-value'>" + acquired + "</span><hr>";
-  return sceneHtml;  
+    "</span><br><span class='text-key'>Acquired:</span> <span class='text-value'>" + acquired + 
+    "</span><br><span class='text-key'>Estimated cloud cover:</span> <span class='text-value'>" + cloudCover + 
+    "</span><br>" +
+    "<span class='text-links'>" +
+    "<a class='hidden' href='#'><span class='glyphicon glyphicon-map-marker' aria-hidden='true'></span> &nbsp;Identify on map</a>" +
+    " | " +
+    "<a target='_blank' href='" + sceneObject.properties.links.thumbnail + 
+    "'><span class='glyphicon glyphicon-download-alt' aria-hidden='true'></span> &nbsp;thumbnail</a>" +
+    " | " +
+    "<a target='_blank' href='" + sceneObject.properties.links.full + 
+    "'><span class='glyphicon glyphicon-download-alt' aria-hidden='true'></span> &nbsp;GeoTIFF</a></span>" + 
+    "<hr>";
+  return sceneHtml;
 }
+
+function toggleSceneAcquiredOrder(){
+  if(sortOrder == "desc"){
+    //change to sort asc
+    sortOrder = "asc";
+    d3.select('#info-scene-list').selectAll('.scene-box').sort(function(a,b){
+      return new Date(a.properties.acquired) - new Date(b.properties.acquired);
+    });
+  } else {
+    // change to sort desc
+    sortOrder = "desc";
+    d3.select('#info-scene-list').selectAll('.scene-box').sort(function(a,b){
+      return new Date(b.properties.acquired) - new Date(a.properties.acquired);
+    });
+  }
+}
+
 
 
 // on window resize
